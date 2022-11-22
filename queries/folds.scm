d6c34beab7a4fac49035fa2d70fe2917b6970e5a
Bakhtiyar Gizzat <1@recoursia.com>
2022-11-22 13:28:04 +0600

    Fix folding tags

--- queries/folds.scm
+++ queries/folds.scm
@@ -1,6 +1,6 @@
 [
-    (method_declaration)
-    (function_definition)
-    (anonymous_function_creation_expression)
-    (heredoc)
+  (method_declaration)
+  (function_definition)
+  (anonymous_function_creation_expression)
+  (heredoc)
 ] @fold
